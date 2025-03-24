0be772600c7525e3962297d904bec7929cb077e4
sadhin28 <taosifsadhin113@gmail.com>
2025-03-25 03:15:51 +0600

    number js exercies

---
+++ number.js
@@ -0,0 +1,17 @@
+var orangePrice = 20;
+var chocolate = '20';
+var banaba =parseInt(34.5);
+var brade=parseFloat(55.9);
+console.log(banaba);
+console.log( typeof banaba);
+console.log(brade);
+console.log(typeof brade);
+// console.log( typeof orangePrice);
+// console.log( typeof chocolate);
+// console.log(orangePrice + chocolate);
+var first =0.1;
+var second = 0.2;
+var total1 = first+second;
+// console.log(total1.toFixed(2));
+const num=(50/0);
+console.log(num);
