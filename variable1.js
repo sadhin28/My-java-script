7fe97f117991b8cf26e6ea51cd9151441a2f6416
sadhin28 <taosifsadhin113@gmail.com>
2025-03-25 02:05:33 +0600

    variable data practice

---
+++ variable1.js
@@ -0,0 +1,11 @@
+//string type
+var name = "sadhin";
+console.log(name);
+var address = "Barishal";
+console.log( typeof address);
+
+//boolean type variable
+var isRich = true;
+console.log(isRich);
+var passed = false;
+console.log( typeof passed);
