a81986d18c1d8d440515194e1cc7fc2ddf5c41f8
sadhin28 <taosifsadhin113@gmail.com>
2025-03-25 01:48:03 +0600

    practic

--- variable.js
+++ variable.js
@@ -1,1 +1,12 @@
 console.log(59)
+/*
+*variable==> vari + able
+ * 5 thins to declare a variable
+ */
+var  weignt = 75;
+var  age    = 23;
+console.log(weignt);
+console.log(age);
+var sum = weignt+age;
+
+console.log(sum+2)
