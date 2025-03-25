4ef4d29f87c04b9f46ff9e39937806f019b27c60
sadhin28 <taosifsadhin113@gmail.com>
2025-03-25 23:46:33 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,41 @@
+/***
+ * different type of comparison
+ * 1.bigger: >
+ * 2.less:<
+ * 3.equal: ==
+ * 4.grater than or equal:>=
+ * 4.less than or equal:<=
+ */
+// console.log(5>10);//false
+// console.log(5<10);//true
+// console.log(5 == 10);//fales
+// console.log(5<=10);//true
+// var peyaraPrice =40;
+// var applePrice =250;
+// console.log(peyaraPrice>applePrice);
+// console.log(peyaraPrice < applePrice);
+// console.log(10 == '10');
+// console.log(10 === '10');
+// console.log(10 != '10');
+// console.log(10 !== '10');
+// const age =12;
+// if(age>10){
+//     console.log('Apni Melay Dhukte parben')
+// }else{
+//     console.log("Sorry, Apnr Boys Kom.Fire Jan")
+// }
+// const age = 14;
+// if (age > 18 || age > 13) {
+//     console.log('Apni Melay Dhukte parben')
+// } else {
+//     console.log("Sorry, Apnr Boys Kom.Fire Jan")
+// }
+// var age =30;
+// if(age >=18){
+//     console.log("Apni jekono ride jete parben");
+// }else if(age>=13){
+//     console.log("Apni kichu raide jete parben");
+// }
+// else{
+//     console.log("Apni shudhu baccader raide jete parben")
+// }
---
+++ problem4.js
@@ -0,0 +1,4 @@
+const b=isNaN(2-10)
+console.log(b)
+const a=isNaN('abc')
+console.log(a)
