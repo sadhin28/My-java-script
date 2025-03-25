ab7a7e3298f63372e2165fcc3bdb2e2997f369cd
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 00:13:28 +0600

    if condition exercies

---
+++ if.js
@@ -0,0 +1,24 @@
+/**
+ * IF you buy food for me
+ *      i will come
+ * 
+ * ----------------------------
+ * if briyani price less than 400 taka
+ * i will buy
+ * 
+ * 
+ * ----------STRUCTURE-------------
+ * if(condition)
+ * */ 
+const salary =25000;
+const isBCS = true;
+const height=70;
+// 
+if(salary > 20000 || height>76){
+    console.log("Eso baba Kobul")
+}
+else{
+    console.log(
+        "vag tui ujbuk"
+    )
+}
