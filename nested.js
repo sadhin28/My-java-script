529b6299d58544150d13b0631ac6bed455b5d0a5
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 03:27:38 +0600

    nested if else

---
+++ nested.js
@@ -0,0 +1,13 @@
+const money = 0;
+if(money>300){
+    console.log("Bro You are rich!!");
+}
+else{
+    if(money>100){
+        console.log("Tui gorib o na Boro lok o na")
+    }else if(money>0){
+        console.log("kola kha")
+    }else{
+        console.log("Tui amar Bondhu Na")
+    }
+}
