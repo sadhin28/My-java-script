687084a31ccab80ab54570c17b96bea03a8c9456
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 17:33:30 +0600

    task 1 complate

---
+++ task1.js
@@ -0,0 +1,12 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+const Burger=200;
+if(Burger>500){
+    console.log("Free Coke");
+}else{
+    console.log("Cock:30 tk");
+}
