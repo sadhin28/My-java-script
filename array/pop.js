ec84bbbb767825d40ff35c92e5ae56c794f36c0e
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 21:34:46 +0600

    pop operation

---
+++ array/pop.js
@@ -0,0 +1,10 @@
+const friends= ['Redwan', 'Rayhan', 'Rean', 'Tahsin' ];
+friends.push('Omor')
+friends.push('Mahin')
+console.log(friends);
+const aout1=friends.pop()
+const aout2=friends.pop()
+const aout3=friends.pop()
+console.log(friends);
+console.log(aout1,aout2, aout3)
+
---
+++ array/push_pop.js
@@ -0,0 +1,6 @@
+const number = [12, 78, 98, 45]
+const age = [];
+number.push(44)
+number.push(68)
+number.push(1, 4, 7)
+console.log(number)
