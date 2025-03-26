d5f37ec95d933013987d42ca134d663ce281d79a
sadhin28 <taosifsadhin113@gmail.com>
2025-03-27 00:10:23 +0600

    isArray proble

--- array/isArray.js
+++ array/isArray.js
@@ -1,8 +1,9 @@
 //check array or not
 
 friends= ['Redwan', 'Rayhan', 'Rean', 'Tahsin' ];
-const num = [];
+const num = ['hi'];
 const food='ros o gol l a';
-console.log(Array.isArray(friends));
-console.log(Array.isArray(food));
-console.log(Array.isArray(num));
+// console.log(Array.isArray(friends));
+// console.log(Array.isArray(food));
+// console.log(Array.isArray(num));
+console.log(friends.concat(num))
