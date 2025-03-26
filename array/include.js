63d79afc79b2008ee467f90fc556115fb443b3bf
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 22:02:36 +0600

    include practice

---
+++ array/include.js
@@ -0,0 +1,8 @@
+friends= ['Redwan', 'Rayhan', 'Rean', 'Tahsin' ];
+console.log(friends.includes('Rayhan'));
+console.log(friends.includes('akter'))
+if(friends.includes('Rean')){
+    console.log('My Friend')
+}else{
+    console.log('Not my friend');
+}
