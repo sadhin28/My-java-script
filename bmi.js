009bd38576b691c1b69c1bbf6ed30a0e865d558c
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 16:33:36 +0600

    bmi calculator

--- bmi.js
+++ bmi.js
@@ -1,4 +1,51 @@
-const weight = parseFloat(prompt("Enter your weight in kg:"));
-const height = parseFloat(prompt("Enter your height in meters:"));
-const bmi = weight / (height * height);
-alert(`Your BMI is: ${bmi.toFixed(2)}`);
+// function calculateBMI() {
+//     const weight = parseFloat(document.getElementById('weight').value);
+//     const height = parseFloat(document.getElementById('height').value);
+    
+//     // Check if inputs are valid
+//     if (!weight || !height || height <= 0) {
+//       document.getElementById('result').innerHTML = 'Please enter valid weight and height values.';
+//       return;
+//     }
+    
+//     // Calculate BMI: weight (kg) / (height (m))^2
+//     const bmi = weight/height*height;
+//     let interpretation = '';
+    
+//     // Interpret the BMI result
+//     if (bmi < 18.5) {
+//       interpretation = 'Underweight';
+//     } else if (bmi < 24.9) {
+//       interpretation = 'Normal weight';
+//     } else if (bmi < 29.9) {
+//       interpretation = 'Overweight';
+//     } else {
+//       interpretation = 'Obesity';
+//     }
+    
+//     document.getElementById('result').innerHTML = 
+//       'Your BMI is ' + bmi.toFixed(2) + ' (' + interpretation + ').';
+//   }
+function calculateBmi(){
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height=parseFloat(document.getElementById('height').value);
+    if(!weight || !height || height <= 0){
+        document.getElementById('result').innerHTML='Place enter valid weight and height.';
+    }
+    const bmi=weight / (height * height);
+    let interpretation= '';
+    
+    if(bmi <18.5){
+        interpretation='Underweight';
+    }
+    else if(bmi > 24.9){
+        interpretation='Normal Weight';
+    }
+    else if(bmi>29.9){
+        interpretation='Overweight';
+    }
+    else{
+        interpretation='Obesity'
+    }
+    document.getElementById('result').innerHTML='Your MBI is' + bmi.toFixed(2) + ' (' + interpretation +').';
+}
