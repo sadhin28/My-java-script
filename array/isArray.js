599a08c25870e1b4cc515807f6446f428e54acd0
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 22:11:48 +0600

    isArray in array

---
+++ array/isArray.js
@@ -0,0 +1,8 @@
+//check array or not
+
+friends= ['Redwan', 'Rayhan', 'Rean', 'Tahsin' ];
+const num = [];
+const food='ros o gol l a';
+console.log(Array.isArray(friends));
+console.log(Array.isArray(food));
+console.log(Array.isArray(num));
