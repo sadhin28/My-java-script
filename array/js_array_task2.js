25817fc9a01a3b1526b3a2eb8f591364d21e8e27
sadhin28 <taosifsadhin113@gmail.com>
2025-03-27 00:27:46 +0600

    jsarraytask2done

---
+++ array/js_array_task2.js
@@ -0,0 +1,16 @@
+/**
+ * Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output
+ */
+let turistDestinations =['Kuyakata', 'Sitakundo', 'Sundorban'];
+console.log( turistDestinations)
+turistDestinations.push('Cottogram');
+console.log( turistDestinations);
+turistDestinations.push('Bandorban', 'Guliyakhali');
+console.log( turistDestinations);
+turistDestinations.pop();
+console.log( turistDestinations);
