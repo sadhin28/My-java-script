8fddd1919bd209d9570ac54021ad92ef3cdebbea
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 19:03:13 +0600

    task4 complate

---
+++ task4.js
@@ -0,0 +1,30 @@
+/***
+
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+const myMarks =85;
+const fdMarks=40
+if (myMarks>80){
+    if(fdMarks>80){
+        console.log("Go for lunch");
+    }else if(fdMarks<80 && fdMarks>=60 ){
+        console.log("good luck next time. ")
+    }
+    else if(fdMarks<60 && fdMarks>=40 ){
+        console.log("keep your friend's message unseen");
+    }
+    else{
+        console.log("block your friend");
+    }
+
+}else{
+    console.log(" go to home and sleep and act sad");
+}
