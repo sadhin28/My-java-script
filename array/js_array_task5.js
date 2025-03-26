56f8a73ae1b48e6faa0f54e5391ddef833bfb0e0
sadhin28 <taosifsadhin113@gmail.com>
2025-03-27 01:12:05 +0600

    jsarray task5 done

---
+++ array/js_array_task5.js
@@ -0,0 +1,11 @@
+/**
+ *  Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+ */
+let book=['java', 'javascript', 'C++', 'EEE'];
+let fruits=['mango', 'Banana', 'pineapple'];
+console.log(book.concat(fruits));
