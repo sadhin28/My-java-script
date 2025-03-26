fbd69d2ece046e32cade513867ca1ceac0d73bd1
sadhin28 <taosifsadhin113@gmail.com>
2025-03-26 21:09:39 +0600

    array practic

---
+++ array/index.js
@@ -0,0 +1,8 @@
+let number =[33, 56, 76, 23, 58, 90 ];
+//get element value by index
+// console.log(number[1] ,number[3])
+//set or update element value by index
+let abc=45;
+abc=99;
+number[1]=45;
+console.log(number);
---
+++ array/length.js
@@ -0,0 +1,2 @@
+let number = [1, 4, 5, 6, 7, 12, 14, 15, 16, 17];
+console.log(number.length)
