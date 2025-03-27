7d2001a475fc6a8d3a5ead20cc76baf607ce7490
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 00:34:21 +0600

    break task3 done

---
+++ js_loop/break_task3.js
@@ -0,0 +1,10 @@
+/**
+ * /* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+for(let i=0;i<=100;i++){
+    console.log(i);
+    if(i>=16){
+        break;
+    }
+}
