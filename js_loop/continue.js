ab1a4463f14aff6698df68afe3f488fec0e29817
sadhin28 <taosifsadhin113@gmail.com>
2025-03-27 23:20:25 +0600

    continue

---
+++ js_loop/continue.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+let i=0;
+while(i<=50){
+    i++;
+    if(i%5 !==0){
+    
+        continue;
+    }
+    console.log(i)
+}
