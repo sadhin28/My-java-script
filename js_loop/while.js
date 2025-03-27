ab1adc206ec61ca416606aa58420c94b365a6804
sadhin28 <taosifsadhin113@gmail.com>
2025-03-27 16:30:55 +0600

    while loop

---
+++ js_loop/while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if(number<1){
+    console.log(
+        "choto rkta number paichi"
+    )
+}
+console.log('Somthing new is cooking');
+//----------------while----loop---------------
+let num=0;
+while(num<=15){
+    console.log(num)
+   num++;
+
+} 
