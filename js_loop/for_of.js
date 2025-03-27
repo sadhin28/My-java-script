a077dac27ba081c9d8c80d6d8c6dfca109de3352
sadhin28 <taosifsadhin113@gmail.com>
2025-03-27 15:55:07 +0600

    forof loop practic

---
+++ js_loop/for_of.js
@@ -0,0 +1,15 @@
+// const name=['Redwan', 'rayhan', 'Rean', 'omor', 'mahin'];
+
+// console.log(name);
+
+// for(let num of name){
+//     console.log(num);
+// }
+
+const numbers=[12, 13, 15, 16, 132, 321];
+numbers.push(777,222)
+numbers.pop()
+for(const num of numbers){
+    console.log(num)
+   
+}
---
+++ js_loop/recap.js
@@ -0,0 +1,6 @@
+const amount = 200;
+if(amount>100){
+    
+}else{
+    
+}
