3ed7ae1d8918caa27ce64d710144441ce5f6e61b
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 00:58:27 +0600

    for task2 done

---
+++ js_loop/for_task2.js
@@ -0,0 +1,28 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+//subtask1
+for(let i=61;i<=100;i+=2){
+    console.log(i);
+}
+
+//subtask2
+for(let i=78;i<=98;i+=2){
+    console.log(i);
+}
