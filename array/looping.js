fd715352359d8ba3c8265a6047c62752bdd35b41
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 02:48:15 +0600

    looping practic

---
+++ array/looping.js
@@ -0,0 +1,18 @@
+/***
+ * Looping Technique
+ * for loop
+ * while loop
+ * do while loop ----->ignore
+ * for in loop--------->array loop korar jonno use kori
+ * 
+ * foro of loop-------->object loop korar jonno use kori
+ */
+
+const friends=['elon','bill','mark','waren'];
+// for (const friend of friends){
+//     console.log(friend)
+// }
+
+for(let i=0;i<friends.length;i++){
+    console.log(friends[i]);
+}
