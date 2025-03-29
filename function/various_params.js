70c29d429276970dbd8d07b9fff3ed2e144440b5
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 21:22:12 +0600

    various function added

---
+++ function/various_params.js
@@ -0,0 +1,18 @@
+function evenSizedString(str){
+    // console.log(str);
+    const size = str.length;
+    
+    if(size%2===0){
+        console.log('Even')
+      
+    }
+    else{
+        console.log('Odd')
+        
+       
+    }
+    console.log(str , size)
+
+}
+evenSizedString('Dhake')
+evenSizedString('lulu')
