6a2509c4bfc928cbc7d86bc753e4fba6eb5ad78f
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 15:04:23 +0600

    function exercies

---
+++ function/function.js
@@ -0,0 +1,17 @@
+const number = 45;
+//function declaration
+function fanOffkor(){
+    console.log('Bosa theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off the fan');
+}
+
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min goso tar pore pic fello')
+}
+
+//call function
+fanOffkor();
+brushTeeth();
