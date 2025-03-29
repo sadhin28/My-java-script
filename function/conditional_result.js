92e276d46f492574771cbc29c32ef853c1238c31
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 17:49:57 +0600

    conditional return

---
+++ function/conditional_result.js
@@ -0,0 +1,21 @@
+function ieEven(number){
+    if(number%2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+console.log(ieEven(5))
+console.log(ieEven(10))
+
+function isOdd(number){
+    if(number%2===1){
+        return true;
+    }
+    return false;
+}
+console.log(isOdd(10))
+console.log(isOdd(5))
