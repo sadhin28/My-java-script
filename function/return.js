1fbc69102c4e38364ff29bd6615d4025b7257e8b
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 17:27:51 +0600

    return example

---
+++ function/return.js
@@ -0,0 +1,14 @@
+function TenTimes (number){
+    const result =number*10;
+    return result;
+}
+
+function cutHalf (num){
+    const half=num/2;
+    return half;
+}
+const tentime=TenTimes(5);
+console.log(tentime)
+
+const half=cutHalf(5);
+console.log(half)
