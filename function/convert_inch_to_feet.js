42b242d3b72910268d8d9e925083b0b67b2e7ac0
sadhin28 <taosifsadhin113@gmail.com>
2025-03-30 15:28:11 +0600

    convert inch to feet

---
+++ function/convert_inch_to_feet.js
@@ -0,0 +1,14 @@
+//we know 12 inch 1 feet
+
+function inchTofeet(inch){
+    const feet = inch/12;
+   
+    
+    const feets=parseInt(feet)
+    return feets  ;
+
+}
+
+console.log(inchTofeet(75))
+
+
