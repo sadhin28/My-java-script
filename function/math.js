ea692d52ad93572c84fe8db19c89cd5b2dd2a553
sadhin28 <taosifsadhin113@gmail.com>
2025-03-31 00:17:09 +0600

    math practic

---
+++ function/math.js
@@ -0,0 +1,13 @@
+const min = Math.min(34,23,1,12,14,87);
+const max = Math.max(34,23,1,12,14,87);
+console.log(min);
+console.log(max);
+console.log(Math.PI.toFixed(4))
+console.log(Math.abs(5-10))
+console.log(Math.round(8.5))
+console.log(Math.floor(8.5))
+console.log(Math.ceil(8.5))
+
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
