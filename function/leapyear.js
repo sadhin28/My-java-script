88310ab592357064330ddca18c1e9258435527c4
sadhin28 <taosifsadhin113@gmail.com>
2025-03-30 16:37:53 +0600

    check leapyear

---
+++ function/leapyear.js
@@ -0,0 +1,10 @@
+function leapyear(year){
+    if(year%100 !==0 && year % 4 === 0 || year%400 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+  
+}
+console.log(leapyear(2052))
