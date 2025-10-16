bff0a0487e5f1eb2443e94223338d26efc688371
sadhin28 <taosifsadhin113@gmail.com>
2025-10-16 19:14:35 +0600

    compare x and y

--- Interview/Question6.js
+++ Interview/Question6.js
@@ -3,3 +3,11 @@ let a;
 console.log(typeof(a));//undefined
 let b=null;
 console.log(typeof(b))//Object
+let x;
+let y = null;
+
+console.log(x); // undefined
+console.log(y); // null
+
+console.log(x == y);  // true (value comparison)
+console.log(x === y); // false (type + value comparison)
