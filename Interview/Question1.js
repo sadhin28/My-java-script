e7b2c263bbb974c098926d0dfca7dee93cdb5e79
sadhin28 <taosifsadhin113@gmail.com>
2025-10-16 18:10:37 +0600

    explain the difference between == and == in javascript

--- Interview/Question1.js
+++ Interview/Question1.js
@@ -3,6 +3,9 @@ const  arr=[1,2,3,4,2,3,5,6,7,5];
 const uniquearr =[...new Set(arr)]
 
 //print duplicate value
+
 console.log(arr)
+
 //print without duplicate value
 console.log(uniquearr)
+
---
+++ Interview/Question2.js
@@ -0,0 +1,5 @@
+//Q2: Explain the different between == and === in JavaScript
+const a = 5== '5'//Ans:True
+console.log(a)
+const b= 5 === '5'//Ans:false
+console.log(b)
