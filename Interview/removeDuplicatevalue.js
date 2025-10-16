95f91c793e2ad884dcb8bc1be50b9edf1c3254bf
sadhin28 <taosifsadhin113@gmail.com>
2025-10-16 18:03:40 +0600

    add remove duplicate value

---
+++ Interview/removeDuplicatevalue.js
@@ -0,0 +1,8 @@
+// Q1. Remove Duplicate Value 
+const  arr=[1,2,3,4,2,3,5,6,7,5];
+const uniquearr =[...new Set(arr)]
+
+//print duplicate value
+console.log(arr)
+//print without duplicate value
+console.log(uniquearr)
